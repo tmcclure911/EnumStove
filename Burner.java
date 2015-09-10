97fa8dd19052f8083c0643739ba48a0aa81469b9
Taylor McClure <tmcclure@mymail.mines.edu>
2015-09-10 11:50:50 -0600

    Initial Commit

---
+++ Burner.java
@@ -0,0 +1,23 @@
+
+public class Burner {
+	private enum Temperature {HOT, WARM, COLD};
+	
+	Temperature temperature;
+	Setting setting;
+	private int timer = 0;
+	private static final int TIME_DURATION = 2;
+	
+	public Temperature getTemperature() {
+		return temperature;
+	}
+	
+	private void increaseSetting(){
+		
+	} 
+	private void decreaseSetting(){
+		
+	}
+	private void updateTemperature(){
+		
+	}
+}
---
+++ Setting.java
@@ -0,0 +1,14 @@
+
+public enum Setting {
+	OFF("---"), LOW("--+"), MEDIUM("-++"), HIGH("+++");
+	
+	private String value;
+	
+	Setting (String aValue) {
+		value = aValue;
+	}
+	
+	public String toString() {
+		return value;
+	}
+}
